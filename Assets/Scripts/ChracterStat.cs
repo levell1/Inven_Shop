df7fc3c31432df605dc53f11d55d628bca14a2c0
levell1 <whddnr0317@naver.com>
2023-12-11 15:10:03 +0900

    [Init]

---
+++ Assets/Scripts/ChracterStat.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChracterStat : MonoBehaviour
+{
+    string _id = "name";
+    string _job = "±â»ç";
+    int _level = 1;
+    int _gold = 1000;
+    int _exp = 0;
+    int _maxExp = 10;
+
+    void ChracterStat() 
+    {
+
+    }
+}
